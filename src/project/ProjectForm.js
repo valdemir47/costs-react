74ecbf61270046af5c9923f6c5d7b2436b7a2e81
Valdemir47 <juniorsantos2016dm@gmail.com>
2025-11-12 11:00:50 -0300

    21-Componetização de formulario

--- src/project/ProjectForm.js
+++ src/project/ProjectForm.js
@@ -1,27 +1,44 @@
 import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css' 
 
-function ProjectForm(){
+//btnText (pegando propriedade do componente pai)
+function ProjectForm({btnText}){
     return(
         <form className={styles.form}>
-            
+            {/*Input/forms com componetização*/}
             <Input 
                 type ="text" 
                 text="Nome do Projeto" 
                 name="name" 
                 placeholder="Insira o nome do Projeto" 
                 />
-            <div>
+            <Input 
+                type ="number" 
+                text="Orçamento do Projeto" 
+                name="budget" 
+                placeholder="Insira o orçamento total" 
+                />
+
+            <Select name="category_id" text="Selecione a categoria"/>
+
+            <SubmitButton text={btnText}/> {/*pegando do elemento pai/paramento funcao*/}
+
+             {/*<div>
                 <input type="number" placeholder="Insira o Orçamento total"/>
-            </div>
+            </div>input sem componetização*/}
+            
+            {/*
             <div>
                 <select name="category_id">
                 <option disabled> Selecione a Categoria </option>
             </select>
-            </div>
-            <div>
-                <input type="submit" value="criar projeto"/>
-            </div>
+            </div> 
+            select sem componetização*/}
+            
+            
+            
         </form>
     ) 
     
